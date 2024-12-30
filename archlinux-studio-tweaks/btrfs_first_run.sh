3951245e85983cb6ec0883a1491020e3158ddfa9
usrmusicman <linuxguy93@gmail.com>
2024-12-30 14:40:14 -0500

    Remove btrfs snapper script

--- archlinux-studio-tweaks/btrfs_first_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [[ "$USER" == "root" ]]; then
-pacman -Syyuu --noconfirm grub-btrfs inotify-tools snapper
-systemctl enable grub-btrfsd.service
-snapper -c root create-config /
-chmod 750 /.snapshots
-chown :wheel /.snapshots
-rm first_run.sh
-fi
